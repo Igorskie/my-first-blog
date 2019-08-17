431a35144803cb9768e597d945116c94ced6ea13
Your Name <you@example.com>
2019-08-17 16:03:57 +0800

    updated.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm 
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 

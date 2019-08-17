22b6961de29b5419bb6ea1b8cfce1fee9eeabca5
Your Name <you@example.com>
2019-08-17 15:46:22 +0800

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
